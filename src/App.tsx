ce65b42dab76f9cd91d493efec343012106a20fc
Radoslaw Czerwiec <radoslaw.czerwiec@ventogroup.com>
2023-08-18 09:48:19 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from "./components/Menu/Menu";
 
 function App() {
   return (
     <div className="App">
+      <Menu></Menu>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        header
       </header>
+      <main className='App-main'>
+        main
+      </main>
+      <footer className='App-footer'>
+        footer
+      </footer>
     </div>
   );
 }
